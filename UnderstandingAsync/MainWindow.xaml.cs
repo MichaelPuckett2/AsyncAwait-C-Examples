23a5e016f1619af3c30b4731f364c4d4f1d2bded
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-03-26 17:42:52 -0400

    Updated for readability and clarification.

--- UnderstandingAsync/MainWindow.xaml.cs
+++ UnderstandingAsync/MainWindow.xaml.cs
@@ -6,10 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace UnderstandingAsync
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{  
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,56 +24,61 @@ namespace UnderstandingAsync
             Application.Current.Dispatcher.Invoke(() => TextBlock_Timer.Text = $"{now.Second}.{now.Millisecond}");
         }
 
-        private async void String_WithAsynLabel_Click(object sender, RoutedEventArgs e)
+        private async void StrinTask_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            if (button == null)
-                return;
 
-            button.IsEnabled = !(CheckBox_DisableButton.IsChecked).GetValueOrDefault(false);
-            ListBoxResults.Items.Add(await TaskExamples.String_WithAsynLabel());
+            if ((CheckBox_DisableButton.IsChecked).GetValueOrDefault())
+                button.IsEnabled = false;
+
+            ListBoxResults.Items.Add(await TaskExamples.StringTask());
+
             button.IsEnabled = true;
         }
 
-        private async void String_WithTaskRun_Click(object sender, RoutedEventArgs e)
+        private async void AsyncStringTask_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            if (button == null)
-                return;
 
-            button.IsEnabled = !(CheckBox_DisableButton.IsChecked).GetValueOrDefault(false);
-            ListBoxResults.Items.Add(await TaskExamples.String_WithTaskRun());
+            if (CheckBox_DisableButton.IsChecked.GetValueOrDefault())
+                button.IsEnabled = false;
+
+            ListBoxResults.Items.Add(await TaskExamples.AsyncStringTask());
+
             button.IsEnabled = true;
-        }    
+        }
 
-        private async void String_WithTaskFromResultAsWhole_Click(object sender, RoutedEventArgs e)
+        private async void StringTaskDotRun_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            if (button == null)
-                return;
 
-            button.IsEnabled = !(CheckBox_DisableButton.IsChecked).GetValueOrDefault(false);
-            ListBoxResults.Items.Add(await TaskExamples.String_WithTaskFromResultAsWhole());
+            if (CheckBox_DisableButton.IsChecked.GetValueOrDefault())
+                button.IsEnabled = false;
+
+            ListBoxResults.Items.Add(await TaskExamples.StringTaskDotRun());
+
             button.IsEnabled = true;
         }
 
         private async void RunAll_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            if (button == null)
-                return;
 
-            button.IsEnabled = !(CheckBox_DisableButton.IsChecked).GetValueOrDefault(false);
+            if (CheckBox_DisableButton.IsChecked.GetValueOrDefault())
+                button.IsEnabled = false;
 
             var tasks = new List<Task<string>>()
             {
-                TaskExamples.String_WithAsynLabel(),
-                TaskExamples.String_WithTaskRun(),
-                TaskExamples.String_WithTaskFromResultAsWhole()
+                TaskExamples.StringTask(),
+                TaskExamples.StringTaskDotRun(),
+                TaskExamples.AsyncStringTask()
             };
 
             foreach (var task in tasks)
-                ListBoxResults.Items.Add(await task);
+            {
+                var str = await task;
+                ListBoxResults.Items.Add(str);
+            }
 
             /* The ForEach lambda expression required it's own async / await as shown below.
             * If you uncomment the line below and comment out the foreach (var task...) above then you'll notice the button
--- UnderstandingAsync/TaskExamples.cs
+++ UnderstandingAsync/TaskExamples.cs
@@ -1,48 +1,44 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace UnderstandingAsync
 {
     static public class TaskExamples
     {
-
         static int TaskCounter = 0;
-
         static int sleepTime = 1000;
+
         static public int SleepTime
         {
             get { return sleepTime; }
             set { sleepTime = value; }
         }
-        
 
-        static public Task<string> String_WithAsynLabel() 
-            => SubTaskForTaskFromResult(nameof(String_WithAsynLabel));
+        //Task
+        //This continues on the main thread.
+        static public Task<string> StringTask() => SubTaskForTask(nameof(StringTask)); //Blocking
 
-        static async public Task<string> String_WithTaskFromResultAsWhole()
-            => await Task.FromResult(await SubTaskForTaskFromResult(nameof(String_WithTaskFromResultAsWhole)));
+        //async Task
+        static async public Task<string> AsyncStringTask() => await SubTaskForTask(nameof(AsyncStringTask)); //Blocking
 
-        
-        static public Task<string> String_WithTaskRun() //This is the only task that appears to work as it should.
-            //This method actually runs on a background thread where the others are continuations of the main thread. !!Important
-            => Task.Run(async () => await SubTaskForTaskFromResult(nameof(String_WithTaskRun)));
+        //Task.Run
+        //This method actually runs on a background thread where the others are continuations of the main thread.
+        static public Task<string> StringTaskDotRun() => Task.Run(() => SubTaskForTask(nameof(StringTaskDotRun))); //Non-Blocking
 
-     
-        static private async Task<string> SubTaskForTaskFromResult(string taskName)
+       /*This task is broken on purpose.
+        * Notice that when you call the task it will continue processing on the main thread until for the 1st second.
+        * Then it will lock the thread for another second.
+        * This is because the task is running on the same thread that it is called on regardless of async await.
+        * The only way to get this task to operate without blocking the calling thread (most like the UI / Main thread) is to use Task.Run
+        * There are 3 wrapper methods above that call this task in various ways to illustrate the difference.
+        */
+        static private async Task<string> SubTaskForTask(string taskName)
         {
-            //System.Threading.Thread.Sleep(sleepTime);  //When sleeping here the only time it works is if we use the background thread. ... Task.Run(..
+            await Task.Delay(sleepTime / 2);
 
-            await Task.Delay(sleepTime);  //When sleeping here all methods continue because this sleeps on a background thread I'm assuming.
+            Thread.Sleep(sleepTime / 2);
 
             return $"Task {++TaskCounter}:  {taskName}";
         }
-
-        /* NOTE:  In the previous push of this repo I had all Task methods marked with async and they were awaiting the task SubTaskForTaskFromResult.
-         * This didn't work out and the only task that was truly awaited was String_WithTaskRun which returns Task.Run(...
-         * After updating the code to simply await the Task I now understand that the Task is executed on await and processed seperately than the original call.
-         * I can see this in the MainWindow code behind where I am calling each method via await for button press.  The UI timer will continue to run for all tasks
-         * although before it was only running for String_WithTask.
-         * Look at the second commit in this repo to see the difference.
-         * */
-
     }
 }
